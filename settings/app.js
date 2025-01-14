faf7750c709899cf028e7924f63d85987b485e99
ursuladdd <anaclarasugajimin14@gmail.com>
2025-01-14 20:19:08 -0300

    Quase concluida

---
+++ settings/app.js
@@ -0,0 +1,28 @@
+
+let botao = document.getElementById("botao");
+
+
+
+botao.addEventListener('click',() => {
+    let email = document.getElementById("email").value;
+    let mensagem = document.getElementById("erro");
+
+    function validarEmail(email) {
+        const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,}$/;
+        return regex.test(email);
+      }
+
+    
+    if (!validarEmail(email)) {
+        mensagem.textContent = "Please provide a valid email address"
+        
+    } else {
+        alert("Thank you ;)")
+    }
+    return
+
+} );
+
+
+
+
